9c51edc17f44ad5d74137611bab8311ee39931d1
adas mahdi <adas@mutant.ae>
2025-03-24 17:10:31 +0800

    dashboard

---
+++ resources/views/partials/back-office/header-admin.blade.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/portal/sidebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/portal/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/portal/portal.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/portal/fileupload.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/portal/table.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/portal/MultiSelect.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/portal/salesreportdashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/portal/pagevisit.css') }}">
+</head>
+
+</script>
+<style>
+    #notificationDropdown {
+        max-height: 500px;
+        overflow-y: auto;
+        background: #eee;
+        border-radius: 10px;
+    }
+
+    #notificationDropdown a {
+        width: 500px;
+        display: flex;
+        justify-content: space-between;
+        padding: 30px;
+    }
+
+    .notification-count {
+        position: absolute;
+        top: -8px;
+        right: 40px;
+        background: #eeee;
+        border-radius: 50%;
+        padding: 1px 6px;
+        font-size: 18px;
+        color: #053b34;
+        font-weight: 900;
+    }
+</style>
+
+<body>
+    <header>
+        <div class="logosec">
+            <div class="logo">
+                <div class="quarter-circle"></div>
+                LMN.
+            </div>
+            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/menu.png') }}" alt="Menu" class="icn burger-menu menuicn"
+                id="menuicn">
+        </div>
+        <div class="message">
+
+            <div class="notifications-menu">
+                <img  loading="lazy"  class="user-icon" onclick="toggleNotification()"
+                    src="{{ asset('assets/img/back-office-img/bell.png') }}" alt="account">
+                <div class="notification-count" id="notificationCount"></div>
+                <div id="notificationDropdown" class="dropdown-content">
+                </div>
+            </div>
+
+            <div class="user-menu">
+                <img  loading="lazy"  class="user-icon" onclick="toggleDropdown()" src="{{ asset('assets/img/back-office-img/user.png') }}"
+                    alt="account">
+                <div id="userDropdown" class="dropdown-content">
+                    @auth
+                        {{-- <a>Profile</a> --}}
+                        <a href="#">Profile</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                        <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                    @endauth
+
+                    @guest
+                        <a href="#">Login</a>
+                    @endguest
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="main-container">
+        <div class="navcontainer">
+            <nav class="nav" style="z-index: 1;">
+                <div class="nav-upper-options">
+                    <div class="nav-option-home option1">
+                        <a href="{{ route('sales.report') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/dashboard.png') }}" class="nav-img"
+                                alt="dashboard">
+                            <h3 style="margin: 0; font-size: 18px;"> Dashboard</h3>
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('users.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/user.png') }}" class="nav-img" alt="articles">
+                            Users
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('about-us.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/info.png') }}" class="nav-img" alt="articles">
+                            About Us
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('values.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/values.png') }}" class="nav-img" alt="articles">
+                            Values
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('contact.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/phone.png') }}" class="nav-img" alt="articles">
+                            Contact Us
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('admin.contact-leads.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/blog.png') }}" class="nav-img" alt="articles">
+                            Contact Leads
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('refunds.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/refund.png') }}" class="nav-img" alt="articles">
+                            Refunds
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('admin.refund-requests.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/cashback.png') }}" class="nav-img" alt="articles">
+                            Refund Requests
+                        </a>
+                    </div>
+                    {{-- <div class="option2 nav-option">
+                        <a href="{{ route('admin.size-guide.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/blog.png') }}" class="nav-img" alt="articles">
+                            Size Guide
+                        </a>
+                    </div> --}}
+                    <div class="option2 nav-option">
+                        <a href="{{ route('terms.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/terms-and-conditions.png') }}" class="nav-img" alt="articles">
+                            Terms and Conditions
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('blogs.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/blogs.png') }}" class="nav-img" alt="articles">
+                            Blogs
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('products.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/box.png') }}" class="nav-img" alt="articles">
+                            Product
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('faq_categories.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/faq-book.png') }}" class="nav-img"
+                                alt="articles">
+                            FAQS Categories
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('faqs.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/faq.png') }}" class="nav-img" alt="articles">
+                            FAQS
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('orders.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/order.png') }}" class="nav-img" alt="articles">
+                            Order Management
+                        </a>
+                    </div>
+                    <div class="option2 nav-option">
+                        <a href="{{ route('visits.index') }}">
+                            <img  loading="lazy"  src="{{ asset('assets/img/back-office-img/tracking.png') }}" class="nav-img" alt="articles">
+                            Track Users
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="main">
+            @yield('content')
+        </div>
+    </div>
+    <script>
+        let menuicn = document.querySelector(".menuicn");
+        let nav = document.querySelector(".navcontainer");
+
+        function updateMenuIcon() {
+            if (window.innerWidth <= 768) {
+                menuicn.src = "{{ asset('assets/img/back-office-img/menu.png') }}";
+            } else {
+                if (nav.classList.contains("navclose")) {
+                    menuicn.src = "{{ asset('assets/img/back-office-img/menu.png') }}";
+                } else {
+                    menuicn.src = "{{ asset('assets/img/back-office-img/close.png') }}";
+                }
+            }
+        }
+
+        menuicn.addEventListener("click", () => {
+            nav.classList.toggle("navclose");
+            updateMenuIcon();
+        });
+
+        updateMenuIcon();
+        window.addEventListener("resize", updateMenuIcon);
+
+        function toggleDropdown() {
+            var dropdown = document.getElementById("userDropdown");
+            dropdown.classList.toggle("show");
+        }
+
+        function toggleNotification() {
+            var dropdown = document.getElementById("notificationDropdown");
+            dropdown.classList.toggle("show");
+        }
+
+        window.onclick = function (event) {
+            if (!event.target.matches('.user-icon')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                for (var i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+
+        // Add active class to selected nav-option
+        const navOptions = document.querySelectorAll('.nav-option a');
+        navOptions.forEach(option => {
+            option.addEventListener('click', function () {
+                navOptions.forEach(nav => nav.parentElement.classList.remove('active'));
+                this.parentElement.classList.add('active');
+            });
+        });
+
+        // Set active class based on the current URL
+        document.querySelectorAll('.nav-option a').forEach(link => {
+            if (window.location.href.includes(link.href)) {
+                link.parentElement.classList.add('active');
+            }
+        });
+    </script>
+    {{-- Link to js files --}}
+    <script src="{{ asset('js/MultiSelect.js') }}"></script>
+</body>
+
+</html>
