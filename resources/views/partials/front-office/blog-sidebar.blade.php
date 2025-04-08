5834069e320ecc03f636c9019448280de29c6ef9
reymar-g <reymar.webdevelopment@gmail.com>
2025-04-09 01:05:19 +0800

    Connect Recent New Articles Section, Connect Categories Section and Fix Filteration

--- resources/views/partials/front-office/blog-sidebar.blade.php
+++ resources/views/partials/front-office/blog-sidebar.blade.php
@@ -1,3 +1,8 @@
+@php
+    use Illuminate\Support\Str;
+    $currentUrl = request()->path(); 
+@endphp
+
 <div class="sidebar-area mt-none-30">
     <div class="widget mt-30 blog-sidebar-widget blog-sidebar-widget-search">
         <h3 class="widget__title blog-sidebar-title">Search</h3>
@@ -6,54 +11,84 @@
             <button><i class="far fa-search"></i></button>
         </form>
     </div>
-    <div class="widget mt-30 blog-sidebar-widget blog-sidebar-widget-categories">
-        <h3 class="widget__title blog-sidebar-title">Categories</h3>
-        <ul class="widget__category">
-            <li class="blog-sidebar-categories-content pb-15"><a href="#!">Dubai Properties<span>05</span></a></li>
-            <li class="blog-sidebar-categories-content pb-15"><a href="#!">Cryptocurrency Mogul<span>03</span></a></li>
-            <li class="blog-sidebar-categories-content pb-15"><a href="#!">Cryptocurrency Investment<span>06</span></a></li>
-            <li class="blog-sidebar-categories-content pb-15"><a href="#!">Luxury Real Estate<span>04</span></a></li>
-            <li class="blog-sidebar-categories-content pb-15"><a href="#!">Blockchain Innovations<span>02</span></a></li>
-            <li class="blog-sidebar-categories-content pb-15"><a href="#!">Digital Asset Management<span>07</span></a></li>
-            <li class="blog-sidebar-categories-content"><a href="#!">Virtual Reality Ventures<span>08</span></a></li>
-        </ul>
-    </div>
-    <div class="widget mt-30 blog-sidebar-widget blog-sidebar-widget-recent">
-        <h3 class="widget__title blog-sidebar-title">Recent Posts</h3>
-        <div class="widget__post">
-            <div class="widget__post-item ul_li blog-widget-post-item">
-                <div class="post-thumb">
-                    <a href="blog-single.html"><img src="{{ asset('/assets/img/blog/post_01.jpg') }}" alt=""></a>
-                </div>
-                <div class="post-content">
-                    <span class="post-date">July 25,2023</span>
-                    <h4 class="post-title border-effect-2"><a href="blog-single.html">We Advocate Swapping Screen
-                            Time</a></h4>
-                </div>
-            </div>
-            <div class="widget__post-item ul_li blog-widget-post-item">
-                <div class="post-thumb">
-                    <a href="blog-single.html"><img src="{{ asset('/assets/img/blog/w_02.jpg') }}" alt=""></a>
-                </div>
-                <div class="post-content">
-                    <span class="post-date">March 20,2023</span>
-                    <h4 class="post-title border-effect-2"><a href="blog-single.html">Utilizing mobile technology in the
-                            field</a></h4>
-                </div>
-            </div>
-            <div class="widget__post-item ul_li blog-widget-post-item">
-                <div class="post-thumb">
-                    <a href="blog-single.html"><img src="{{ asset('/assets/img/blog/w_03.jpg') }}" alt=""></a>
-                </div>
-                <div class="post-content">
-                    <span class="post-date">July 18,2023</span>
-                    <h4 class="post-title border-effect-2"><a href="blog-single.html">Building intelligent
-                            transportation systems</a></h4>
-                </div>
-            </div>
-
+    @if(Str::contains($currentUrl, 'news'))
+        <div class="widget mt-30 blog-sidebar-widget blog-sidebar-widget-categories">
+            <h3 class="widget__title blog-sidebar-title">Categories</h3>
+            <ul class="widget__category">
+                @forelse($categories as $category)
+                    <li class="blog-sidebar-categories-content {{ !$loop->last ? 'pb-15' : '' }}">
+                        <a href="{{ url('news') . '?category=' . urlencode($category->id) }}">
+                            {{ $category->name }} <span>{{ $category->news_count }}</span>
+                        </a>
+                    </li>
+                @empty
+                    <p>No category available.</p>
+                @endforelse
+            </ul>
         </div>
-    </div>
+        <div class="widget mt-30 blog-sidebar-widget blog-sidebar-widget-recent">
+            <h3 class="widget__title blog-sidebar-title">Recent News</h3>
+            <div class="widget__post">
+                @forelse($latestNews as $latestnews)
+                    <div class="widget__post-item ul_li blog-widget-post-item">
+                        <div class="post-thumb blog-desktop-thumb">
+                            <a href="{{ url('news/' . $latestnews->id) }}">
+                                <img src="{{ asset('storage/' . $latestnews->thumbnail) }}" alt="News Image" class="desktop">
+                                <img src="{{ asset('storage/' . $latestnews->mobile_thumbnail) }}" alt="News Image"
+                                    class="mobile">
+                            </a>
+                        </div>
+                        <div class="post-content">
+                            <span class="post-date">{{ \Carbon\Carbon::parse($latestnews->date)->format('M d, Y') }}</s>
+                                <h4 class="post-title border-effect-2"><a
+                                        href="{{ url('news/' . $latestnews->id) }}">{{ $latestnews->title }}</a></h4>
+                        </div>
+                    </div>
+                @empty
+                    <p>No news availabe.</p>
+                @endforelse
+            </div>
+        </div>
+    @elseif(Str::contains($currentUrl, 'articles'))
+        <div class="widget mt-30 blog-sidebar-widget blog-sidebar-widget-categories">
+            <h3 class="widget__title blog-sidebar-title">Categories</h3>
+            <ul class="widget__category">
+                @forelse($categories as $category)
+                    <li class="blog-sidebar-categories-content {{ !$loop->last ? 'pb-15' : '' }}">
+                        <a href="{{ url('articles') . '?category=' . urlencode($category->id) }}">
+                            {{ $category->name }} <span>{{ $category->articles_count }}</span>
+                    </li>
+                @empty
+                    <p>No category available.</p>
+                @endforelse
+            </ul>
+        </div>
+        <div class="widget mt-30 blog-sidebar-widget blog-sidebar-widget-recent">
+            <h3 class="widget__title blog-sidebar-title">Recent Articles</h3>
+            <div class="widget__post">
+                @forelse($latestArticles as $latestarticles)
+                    <div class="widget__post-item ul_li blog-widget-post-item">
+                        <div class="post-thumb blog-desktop-thumb">
+                            <a href="{{ url('articles/' . $latestarticles->id) }}">
+                                <img src="{{ asset('storage/' . $latestarticles->thumbnail) }}" alt="News Image"
+                                    class="desktop">
+                                <img src="{{ asset('storage/' . $latestarticles->mobile_thumbnail) }}" alt="News Image"
+                                    class="mobile">
+                            </a>
+                        </div>
+                        <div class="post-content">
+                            <span class="post-date">{{ \Carbon\Carbon::parse($latestarticles->date)->format('M d, Y') }}</s>
+                                <h4 class="post-title border-effect-2"><a
+                                        href="{{ url('articles/' . $latestarticles->id) }}">{{ $latestarticles->title }}</a>
+                                </h4>
+                        </div>
+                    </div>
+                @empty
+                    <p>No articles availabe.</p>
+                @endforelse
+            </div>
+        </div>
+    @endif
     <div class="widget mt-30 blog-sidebar-widget blog-sidebar-widget-recent">
         <h3 class="widget__title blog-sidebar-title">Follow Us</h3>
         <ul class="widget__social ul_li blog-widget-social">
@@ -64,18 +99,26 @@
             <li><a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a></li>
         </ul>
     </div>
-    <div class="widget mt-30 blog-sidebar-widget-tags">
-        <h3 class="widget__title blog-sidebar-title blog-sidebar-title-tags">Tags</h3>
-        <div class="tagcloud">
-            <a href="#!">Crypto Home</a>
-            <a href="#!">Blockchain</a>
-            <a href="#!">Web</a>
-            <a href="#!">Currency</a>
-            <a href="#!">Crypto Trading</a>
-            <a href="#!">Crypto</a>
-            <a href="#!">Crypto Blockchain</a>
-            <a href="#!">Advisor</a>
-            <a href="#!">Investment</a>
+    @if(Str::contains($currentUrl, 'news'))
+        <div class="widget mt-30 blog-sidebar-widget-tags">
+            <h3 class="widget__title blog-sidebar-title blog-sidebar-title-tags">Tags</h3>
+            <div class="tagcloud">
+                @forelse($tags as $tag)
+                    <a href="#!">{{ $tag->name }}</a>
+                @empty
+                    <p>No tags availabe.</p>
+                @endforelse
+            </div>
         </div>
-    </div>
-</div>
+    @elseif(Str::contains($currentUrl, 'articles'))
+        <div class="widget mt-30 blog-sidebar-widget-tags">
+            <h3 class="widget__title blog-sidebar-title blog-sidebar-title-tags">Tags</h3>
+            <div class="tagcloud">
+                @forelse($tags as $tag)
+                    <a href="#!">{{ $tag->name }}</a>
+                @empty
+                    <p>No tags availabe.</p>
+                @endforelse
+            </div>
+        </div>
+    @endif
